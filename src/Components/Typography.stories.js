c3b794df8e8913f24ff7b6e28a5d92e8fbbab21a
erik pantzar <erik@epantzar.se>
2021-07-22 22:38:38 +0200

    Typography component

---
+++ src/Components/Typography.stories.js
@@ -0,0 +1,17 @@
+import Typography from './Typography'
+
+
+export default {
+	title: 'Component/Typography',
+	component: Typography
+}
+
+
+export const Samples = () => (<div>
+	<Typography variant="h2">Submit your load</Typography>
+
+	<Typography variant="h3">last week you put 75</Typography>
+
+
+
+</div>)
