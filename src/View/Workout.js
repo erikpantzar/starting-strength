3bc75525959ad64bb080c67c69d34256e2d1fc98
erik pantzar <erik@epantzar.se>
2021-09-21 21:26:10 +0200

    Added initial version of nav with some information

--- src/View/Workout.js
+++ src/View/Workout.js
@@ -6,15 +6,15 @@ import { useWorkout, useUpdateWorkout } from "../Context/workout-context";
 const Workout = () => {
 	const { workout } = useParams();
 	const { ...workouts } = useWorkout();
-	const { handleWorkout } =  useUpdateWorkout()
+	const { handleWorkout } = useUpdateWorkout();
 
 	const handleSubmit = (wo, weight) => {
-		handleWorkout(wo, weight)
-	}
+		handleWorkout(wo, weight);
+	};
 
 	return (
 		<div>
-		<h1>{workout}</h1>
+			<h1 style={{ textTransform: "Capitalize" }}>{workout}</h1>
 			<WorkoutForm
 				excercise={workout}
 				weight={workouts[workout]}
