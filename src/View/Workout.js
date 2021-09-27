dd65855578011f02909387ed20d6d03749138543
Joacim Nilsen <joacimnilsen1986@gmail.com>
2021-09-27 14:07:58 +0200

    fixed buttonpositioning

--- src/View/Workout.js
+++ src/View/Workout.js
@@ -4,24 +4,24 @@ import { useParams } from "react-router-dom";
 import { useWorkout, useUpdateWorkout } from "../Context/workout-context";
 
 const Workout = () => {
-	const { workout } = useParams();
-	const { ...workouts } = useWorkout();
-	const { handleWorkout } = useUpdateWorkout();
+  const { workout } = useParams();
+  const { ...workouts } = useWorkout();
+  const { handleWorkout } = useUpdateWorkout();
 
-	const handleSubmit = (wo, weight) => {
-		handleWorkout(wo, weight);
-	};
+  const handleSubmit = (wo, weight) => {
+    handleWorkout(wo, weight);
+  };
 
-	return (
-		<div>
-			<h1 style={{ textTransform: "Capitalize" }}>{workout}</h1>
-			<WorkoutForm
-				excercise={workout}
-				weight={workouts[workout]}
-				onSubmit={handleSubmit}
-			/>
-		</div>
-	);
+  return (
+    <div>
+      <h1 style={{ textTransform: "Capitalize" }}>{workout}</h1>
+      <WorkoutForm
+        excercise={workout}
+        weight={workouts[workout]}
+        onSubmit={handleSubmit}
+      />
+    </div>
+  );
 };
 
 export default Workout;
