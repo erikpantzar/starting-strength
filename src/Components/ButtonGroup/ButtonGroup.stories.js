374a9303e723395b989f89a07f0e24d9ea4a8271
Joacim Nilsen <joacimnilsen1986@gmail.com>
2021-09-29 15:42:03 +0200

    moved files and cleaned up some unused code

--- src/Components/ButtonGroup/ButtonGroup.stories.js
+++ src/Components/ButtonGroup/ButtonGroup.stories.js
@@ -2,16 +2,16 @@ import ButtonGroup from "./ButtonGroup";
 import { BrowserRouter as Router } from "react-router-dom";
 
 export default {
-	title: "Component/ButtonGroup",
-	component: ButtonGroup,
+  title: "Component/ButtonGroup",
+  component: ButtonGroup,
 };
 
 const exampleData = [
-	{ text: "Workout A", url: "/a" },
-	{ text: "Workout B", url: "/b" },
+  { text: "Workout A", url: "/a" },
+  { text: "Workout B", url: "/b" },
 ];
 export const example = () => (
-	<Router>
-		<ButtonGroup links={exampleData} />
-	</Router>
+  <Router>
+    <ButtonGroup links={exampleData} />
+  </Router>
 );
--- src/Components/Typography.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './Typography.css'
-
-const Typography = ({ variant = 'p', color = 'black', styles = {}, children }) => {
-	const element = React.createElement(variant, { className: `Typography-${variant}`}, children)
-	return element
-}
-
-export default Typography
