200fe9d10ddfaf0ea9c84ce93c74a0a2c2b9918c
erik pantzar <erik@epantzar.se>
2021-10-09 15:30:45 +0200

    Update story to match issue

--- src/Components/WorkoutList/WorkoutList.stories.js
+++ src/Components/WorkoutList/WorkoutList.stories.js
@@ -1,26 +1,45 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import WorkoutList from "./WorkoutList";
+import { BrowserRouter as Router } from 'react-router-dom'
+import WorkoutList from './WorkoutList'
 
 export default {
-  title: "Workout/List",
+  title: 'Workout/List',
   component: WorkoutList,
-};
+}
 
 export const Primary = () => (
   <Router>
     <WorkoutList
       workouts={[
         {
-          name: "Squats",
-          status: "WORKOUT_DONE",
+          name: 'Squats',
+          status: 'WORKOUT_DONE',
         },
 
         {
-          name: "Deadlift",
-          status: "",
+          name: 'Deadlift',
+          status: '',
         },
-        { name: "Bench", status: "" },
+        { name: 'Bench', status: '' },
       ]}
     />
   </Router>
-);
+)
+
+export const Secondary = () => (
+  <Router>
+    <WorkoutList
+      workouts={[
+        {
+          name: 'Squats',
+          status: 'WORKOUT_DONE',
+        },
+
+        {
+          name: 'Deadlift',
+          status: 'WORKOUT_DONE',
+        },
+        { name: 'Bench', status: '' },
+      ]}
+    />
+  </Router>
+)
