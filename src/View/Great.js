ca0b879a8e7d84b1a371e19f2799cdc0494bf04f
Joacim Nilsen <joacimnilsen1986@gmail.com>
2021-10-13 22:24:45 +0200

    WiP

--- src/View/Great.js
+++ src/View/Great.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import Typography from "../Components/Typography/Typography";
 import { useWorkout } from "../Context/workout-context";
-import { WORKOUTS_ORDER } from "../contants";
+import { WORKOUTS_ORDER, WORKOUTS_SET_AND_REPS } from "../contants";
 
 const Great = () => {
   const { ...workouts } = useWorkout();
@@ -17,7 +17,27 @@ const Great = () => {
         You did variant {type}, next time you should to the other variant.
       </Typography>
       <Typography>
-        On {exc[0]} you worked on {workouts[exc[0]]} kgs for weight
+        On {exc[0]} you worked on{" "}
+        {workouts[exc[0]] *
+          WORKOUTS_SET_AND_REPS[exc[0]][0] *
+          WORKOUTS_SET_AND_REPS[exc[0]][1]}{" "}
+        kg for weight
+      </Typography>
+
+      <Typography>
+        On {exc[1]} you worked on{" "}
+        {workouts[exc[1]] *
+          WORKOUTS_SET_AND_REPS[exc[1]][0] *
+          WORKOUTS_SET_AND_REPS[exc[1]][1]}{" "}
+        kg for weight
+      </Typography>
+
+      <Typography>
+        On {exc[2]} you worked on{" "}
+        {workouts[exc[2]] *
+          WORKOUTS_SET_AND_REPS[exc[2]][0] *
+          WORKOUTS_SET_AND_REPS[exc[2]][1]}{" "}
+        kg for weight
       </Typography>
     </div>
   );
