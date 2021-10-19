eb6db886381558cdab539c328d8619cd4e4c0d1f
Joacim Nilsen <joacimnilsen1986@gmail.com>
2021-10-20 01:14:39 +0200

    wip

--- src/Components/WorkoutList/WorkoutList.js
+++ src/Components/WorkoutList/WorkoutList.js
@@ -1,4 +1,4 @@
-import { React, useState } from 'react'
+import { React } from 'react'
 import { Link } from 'react-router-dom'
 import doing from './doing-dot.svg'
 import done from './done-dot.svg'
