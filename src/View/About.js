a96ee00cfb86df92d78314b013432e87b6554bb8
Joacim Nilsen <joacimnilsen1986@gmail.com>
2021-10-22 14:54:40 +0200

    Changed from br/ to separate paragraphs

--- src/View/About.js
+++ src/View/About.js
@@ -4,25 +4,27 @@ import BackNav from '../Components/BackNav/BackNav'
 const About = () => (
   <div>
     <BackNav route={{ name: 'Back' }} />
-    <Typography variant="p">
+    <Typography variant="h2">What is Starting strength?</Typography>
+    <Typography>
       Starting Strength is a workout program that gives you the most value for
       time spent in the gym.
-      <br />
-      <br />
+    </Typography>
+    <Typography>
       It is used by both beginners and experienced weight lifters and consist of
       five exercises and each workout is made up of three of them.
-      <br />
-      <br />
+    </Typography>
+
+    <Typography>
       You should do three workouts each week, alternating the variants each
       time.
-      <br />
-      <br />
+    </Typography>
+
+    <Typography>
       After one week you raise 2.5kg on all of the exercises, so start out on a
       low weight, over time you will get stronger and be able to lift more.
-      <br />
-      <br />
-      Dont forget to warm up with lower weights!
     </Typography>
+
+    <Typography>Dont forget to warm up with lower weights!</Typography>
   </div>
 )
 
