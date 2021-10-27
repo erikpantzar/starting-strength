4cb45e520930a072f568d7cbe890dbcacf45c63e
Joacim Nilsen <joacimnilsen1986@gmail.com>
2021-10-27 11:50:28 +0200

    wip

--- src/Components/SSInput/SSInput.js
+++ src/Components/SSInput/SSInput.js
@@ -2,13 +2,16 @@ import './SSInput.css'
 
 const SSInput = ({ weight, onChange }) => {
   return (
-    <input
-      className="SSInput"
-      value={weight}
-      onChange={(event) => {
-        onChange(event.target.value)
-      }}
-    />
+    <div className="SSInput-wrapper">
+      <input
+        className="SSInput"
+        value={weight}
+        onChange={(event) => {
+          onChange(event.target.value)
+        }}
+      />
+      <p>kg</p>
+    </div>
   )
 }
 
