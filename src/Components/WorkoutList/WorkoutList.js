966db65dcc5333b7f594e4c427f8ae01566b365e
Joacim Nilsen <51000125+JoacimNilsen@users.noreply.github.com>
2021-11-01 17:18:14 +0100

    Design workout cards (#60)
    * Starded implementing new design for WorkoutList
    
    * Added actual weights to items, removed redirect when all 3 workouts were finished.
    
    * wip
    
    * Made icons into spans instead of SVGs
    
    * Changed classnames to be more consistent and removed unused code
    
    * Removed NTH child elements
    
    * remade the if statement and added colors to root
    
    Co-authored-by: Erik Pantzar <erik@epantzar.se>

--- src/Components/WorkoutList/WorkoutList.js
+++ src/Components/WorkoutList/WorkoutList.js
@@ -1,33 +1,37 @@
-import React from 'react'
+import { React } from 'react'
 import { Link } from 'react-router-dom'
-import arrow from './WorkoutList-arrow.svg'
-import checkmark from './checkmark.svg'
+import { WORKOUTS_SET_AND_REPS } from '../../contants.js'
 import './WorkoutList.css'
+import { useWorkout } from '../../Context/workout-context'
 
 // const workouts = ['bench', 'squats']
 const WorkoutList = ({ workouts, type }) => {
+  const { ...weightOfExercise } = useWorkout()
+
   return (
     <nav className="WorkoutList">
       {workouts.map((workout) => (
         <Link
-          className={[
-            'WorkoutList-link',
-            workout.state === 'WORKOUT_DONE' ? 'workout_isDone' : null,
-          ].join(' ')}
+          className="WorkoutList-link"
           key={workout.name}
           to={`${type}/${workout.name}`}
         >
-          <span className="WorkoutList-link-text">{workout.name}</span>
-
-          {workout.state === 'WORKOUT_DONE' ? (
-            <div className="WorkoutList-link-icon">
-              <img src={checkmark} alt="checkmark" />
-            </div>
-          ) : (
-            <div className="WorkoutList-link-icon">
-              <img src={arrow} alt="arrow" />
-            </div>
-          )}
+          <span className="WorkoutList-link-text WorkoutList-workout">
+            {workout.name}
+          </span>
+          <span className="WorkoutList-link-text WorkoutList-reps">
+            {WORKOUTS_SET_AND_REPS[workout.name][0]}x
+            {WORKOUTS_SET_AND_REPS[workout.name][1]}
+          </span>
+          <span className="WorkoutList-weight">
+            {weightOfExercise[workout.name]}kg
+          </span>
+          <div
+            className={[
+              'WorkoutList-status-icon',
+              workout.state && 'WorkoutList-status-icon--isDone',
+            ].join(' ')}
+          />
         </Link>
       ))}
     </nav>
@@ -35,3 +39,5 @@ const WorkoutList = ({ workouts, type }) => {
 }
 
 export default WorkoutList
+
+//
