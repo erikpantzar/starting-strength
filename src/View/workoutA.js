482baf9e1963955e1d47cb3a0cd8d0238bda8e5a
Joacim Nilsen <51000125+JoacimNilsen@users.noreply.github.com>
2021-11-05 15:47:58 +0100

    Issue/56 new buttons (#64)
    * Started designing buttons with variants
    
    * fixed positioning of the text

--- src/View/workoutA.js
+++ src/View/workoutA.js
@@ -29,8 +29,8 @@ const Workout = () => {
       <Typography variant="h1">{sentence()}</Typography>
       <WorkoutList workouts={workouts[type === 'a' ? 0 : 1]} type={type} />
 
-      <Button onClick={() => reset()} variant="ghost">
-        Reset workout
+      <Button onClick={() => reset()} variant="reset">
+        Reset Workout
       </Button>
     </div>
   )
